efaef59c1278c8bac4d175176b166cb87a2c9f9a
nicobojacarojas <nicobojacarojas@hotmail.com>
2018-05-08 16:59:39 -0500

    segunda version

--- NB_MontyHall.py
+++ NB_MontyHall.py
@@ -1,2 +1,20 @@
 import numpy as np
-
+import random
+def sort_doors():
+	a = ['goat', 'goat', 'car' ]
+	random.shuffle(a)
+	return (a)
+def choose_door():	
+	a = [0, 1, 2]
+	sel = [random.choice(a)]
+	return sel[0]
+def reveal_door (lista, choice):
+	contador = 0;
+	for i in range(len(lista)):
+		if(contador < 1):
+			if (choice!=i):
+				if (lista[i] == 'goat'):
+					lista[i] = 'GOAT_MONTY'
+					contador += 1
+	return lista
+def finish_game 
